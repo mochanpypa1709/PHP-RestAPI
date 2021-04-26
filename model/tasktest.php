c6afe4757c0cf76d2a0417270edca3c68603b355
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-27 01:04:48 +0700

    Create tasktest.php

---
+++ model/tasktest.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('task.php');
+
+try {
+    $task = new Task(1, "Title Here", "Description Here", "27/04/2021 12:00", "N");
+    header('Content-type: application/json;charset=UTF-8');
+    echo json_encode($task->returnTaskAsArray());
+}
+catch(TaskException $ex ) {
+    echo "Error: ".$ex->getMessage();
+}
