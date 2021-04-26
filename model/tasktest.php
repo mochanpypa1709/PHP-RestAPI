2c7aaa3753d7eb06daae7d1fda96292953b16fcc
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-27 01:08:09 +0700

    Update tasktest.php

--- model/tasktest.php
+++ model/tasktest.php
@@ -3,7 +3,7 @@
 require_once('task.php');
 
 try {
-    $task = new Task(1, "Title Here", "Description Here", "27/04/2021 12:00", "N");
+    $task = new Task(1, "Title Here", "Description Here", "27/04/2021 12:00", "");
     header('Content-type: application/json;charset=UTF-8');
     echo json_encode($task->returnTaskAsArray());
 }
