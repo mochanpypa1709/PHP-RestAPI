5e07b9720e2c19b81130b9a6aa265b556b49b8c5
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-27 00:05:10 +0700

    Create dbtest.php

---
+++ controller/dbtest.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('db.php');
+require_once('../model/Response.php');
+
+try{
+    $writeDB = DB::connectWriteDB();
+    $readDB = DB::connectReadDB();
+}
+catch(PDOException $ex ) {
+    $response = new Response();
+    $response->setHttpStatusCode(500);
+    $response->setSuccess(false);
+    $response->addMessage("Database Connection Error");
+    $response->send();
+    exit;
+}
