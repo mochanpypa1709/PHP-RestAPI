9da21997dec8c493633d5a7ee518ea77758b9a1d
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-05-02 23:26:22 +0700

    Update task.php

--- controller/task.php
+++ controller/task.php
@@ -18,6 +18,96 @@ catch (PDOException $ex) {
     exit();
 }
 
+// BEGIN OF AUTH SCRIPT
+// Authenticate user with access token
+// check to see if access token is provided in the HTTP Authorization header and that the value is longer than 0 chars
+// don't forget the Apache fix in .htaccess file
+if(!isset($_SERVER['HTTP_AUTHORIZATION']) || strlen($_SERVER['HTTP_AUTHORIZATION']) < 1)
+{
+  $response = new Response();
+  $response->setHttpStatusCode(401);
+  $response->setSuccess(false);
+  (!isset($_SERVER['HTTP_AUTHORIZATION']) ? $response->addMessage("Access token is missing from the header") : false);
+  (strlen($_SERVER['HTTP_AUTHORIZATION']) < 1 ? $response->addMessage("Access token cannot be blank") : false);
+  $response->send();
+  exit;
+}
+
+// get supplied access token from authorisation header - used for delete (log out) and patch (refresh)
+$accesstoken = $_SERVER['HTTP_AUTHORIZATION'];
+
+// attempt to query the database to check token details - use write connection as it needs to be synchronous for token
+try {
+  // create db query to check access token is equal to the one provided
+  $query = $writeDB->prepare('select userid, accesstokenexpiry, useractive, loginattempts from tblsessions, tblusers where tblsessions.userid = tblusers.id and accesstoken = :accesstoken');
+  $query->bindParam(':accesstoken', $accesstoken, PDO::PARAM_STR);
+  $query->execute();
+
+  // get row count
+  $rowCount = $query->rowCount();
+
+  if($rowCount === 0) {
+    // set up response for unsuccessful log out response
+    $response = new Response();
+    $response->setHttpStatusCode(401);
+    $response->setSuccess(false);
+    $response->addMessage("Invalid access token");
+    $response->send();
+    exit;
+  }
+  
+  // get returned row
+  $row = $query->fetch(PDO::FETCH_ASSOC);
+
+  // save returned details into variables
+  $returned_userid = $row['userid'];
+  $returned_accesstokenexpiry = $row['accesstokenexpiry'];
+  $returned_useractive = $row['useractive'];
+  $returned_loginattempts = $row['loginattempts'];
+  
+  // check if account is active
+  if($returned_useractive != 'Y') {
+    $response = new Response();
+    $response->setHttpStatusCode(401);
+    $response->setSuccess(false);
+    $response->addMessage("User account is not active");
+    $response->send();
+    exit;
+  }
+
+  // check if account is locked out
+  if($returned_loginattempts >= 3) {
+    $response = new Response();
+    $response->setHttpStatusCode(401);
+    $response->setSuccess(false);
+    $response->addMessage("User account is currently locked out");
+    $response->send();
+    exit;
+  }
+
+  // check if access token has expired
+  if(strtotime($returned_accesstokenexpiry) < time()) {
+    $response = new Response();
+    $response->setHttpStatusCode(401);
+    $response->setSuccess(false);
+    $response->addMessage("Access token has expired");
+    $response->send();
+    exit;
+  }  
+}
+catch(PDOException $ex) {
+  $response = new Response();
+  $response->setHttpStatusCode(500);
+  $response->setSuccess(false);
+  $response->addMessage("There was an issue authenticating - please try again");
+  $response->send();
+  exit;
+}
+
+// END OF AUTH SCRIPT
+
+// within this if/elseif statement, it is important to get the correct order (if query string GET param is used in multiple routes)
+
 if(array_key_exists("taskid",$_GET)) {
     $taskid = $_GET['taskid'];
 
